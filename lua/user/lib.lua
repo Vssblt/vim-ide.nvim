2d14d3b4e9db5a6bdbfbc854659cbfbbbd828d48
Vssblt <sunruiyangcp01@gmail.com>
2023-08-18 13:36:17 +0800

    add efm

--- lua/user/lib.lua
+++ lua/user/lib.lua
@@ -122,6 +122,9 @@ function Lib.setup()
   vim.o.foldlevelstart = 99
   vim.o.foldenable = true
   vim.o.splitkeep = "screen"
+  vim.o.efm = "%\\S%#/ld: %f:%l:%m,%*[^\"]\"%f\"%*\\D%l: %m,\"%f\"%*\\D%l: %m,%-G%f:%l: (Each undeclared identifier is reported only once,%-G%f:%l: for each function it appears in.),%-GIn file included from %f:%l:%c:,%-GIn file included from %f:%l:%c\\,,%-GIn file included from %f:%l:%c,%-GIn file included from %f:%l,%-G%*[ ]from %f:%l:%c,%-G%*[ ]from %f:%l:,%-G%*[ ]from %f:%l\\,,%-G%*[ ]from %f:%l,%f:%l:%c:%m,%f(%l):%m,%f:%l:%m,\"%f\"\\, line %l%*\\D%c%*[^ ] %m,%D%*\\a[%*\\d]: Entering directory %*[`']%f',%X%*\\a[%*\\d]: Leaving directory %*[`']%f',%D%*\\a: Entering directory %*[`']%f',%X%*\\a: Leaving directory %*[`']%f',%DMaking %*\\a in %f,%f|%l| %m"
+
+  -- vim.o.efm = "" .. vim.o.efm
   -- vim.opt.foldmethod = "expr"
   -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
   vim.cmd[[colorscheme sonokai]]
